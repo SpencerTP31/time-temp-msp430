b9dc35737750af5c8960a9c5c2be36c41fe9eb5e
Nathan Rosenberg <robotincad@gmail.com>
2018-10-01 18:40:16 -0400

    Works

--- main.c
+++ main.c
@@ -1,107 +1,289 @@
-/************** ECE2049 DEMO CODE ******************/
-/**************  22 August 2018   ******************/
-/***************************************************/
-
 #include <msp430.h>
-
-/* Peripherals.c and .h are where the functions that implement
- * the LEDs and keypad, etc are. It is often useful to organize
- * your code by putting like functions together in files.
- * You include the header associated with that file(s)
- * into the main file of your project. */
 #include "peripherals.h"
+#include "stringstuff.h"
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef enum {S_RUN, S_EDIT} state_t;
+typedef enum {E_MON, E_DAY, E_HR, E_MIN, E_SEC} editstate_t;
 
 // Function Prototypes
 void swDelay(char numLoops);
+void adc_convert(void);
+void runtimerA2(void);
+void stoptimerA2(void);
+void a2delay(float numLoops);
+void toArray(int i, char *s);
+char displayTemp(float rawTemp, int tempType);
+int read_push_button(void);
+//void displayStuff(int count);
 
 // Declare globals here
 
-// Main
-void main(void)
+#define CALADC12_15V_30C  *((unsigned int *)0x1A1A)
+#define CALADC12_15V_85C  *((unsigned int *)0x1A1C)
 
-{
-    unsigned char currKey=0, dispSz = 3;
-    unsigned char dispThree[3];
+struct tm * global_time; //Global time counter.
+struct tm * last_global_time; //Global time counter.
+volatile unsigned int in_temp;
+volatile unsigned int in_wheel;
+volatile char temp_changed = 0;
+unsigned long times[60];
+float tempC[60];
+unsigned int timercount;
+unsigned int delay_cnt = 0;
+char* time_str;
+char* last_time_str;
+char* date_str;
+char* temp_c_str;
+char* temp_f_str;
+unsigned long utc;
 
-    // Define some local variables
-    float a_flt = 190.68;
-    int  test = 0x0600, i=0;     // In C prefix 0x means the number that follows is in hex
-    long unsigned X= 123456;    // No prefix so number is assumed to be in decimal
-    unsigned char myGrade='A';
-    unsigned char initial='S';
-    //unsigned char your_name[14] = "Your Name Here";
-                                    // What happens when you change the array length?
-                                    // What should it be? Do you need null terminator /n ?
-
-
-    WDTCTL = WDTPW | WDTHOLD;    // Stop watchdog timer. Always need to stop this!!
-                                 // You can then configure it properly, if desired
-
-    // Some utterly useless instructions -- Step through them
-    // What size does the Code Composer MSP430 Compiler use for the
-    // following variable types? A float, an int, a long integer and a char?
-    a_flt = a_flt*test;
-    X = test+X;
-    test = test-myGrade;    // A number minus a letter?? What's actually going on here?
-                            // What value stored in myGrade (i.e. what's the ASCII code for "A")?
-                            // Thus, what is the new value of test? Explain?
-
-    // Useful code starts here
-    initLeds();
-
-    configDisplay();
-    configKeypad();
-
-    // *** Intro Screen ***
-    Graphics_clearDisplay(&g_sContext); // Clear the display
-
-    // Write some text to the display
-    Graphics_drawStringCentered(&g_sContext, "Welcome", AUTO_STRING_LENGTH, 48, 15, TRANSPARENT_TEXT);
-    Graphics_drawStringCentered(&g_sContext, "to", AUTO_STRING_LENGTH, 48, 25, TRANSPARENT_TEXT);
-    Graphics_drawStringCentered(&g_sContext, "ECE204-A18!", AUTO_STRING_LENGTH, 48, 35, TRANSPARENT_TEXT);
-
-    // Draw a box around everything because it looks nice
-    Graphics_Rectangle box = {.xMin = 5, .xMax = 91, .yMin = 5, .yMax = 91 };
-    Graphics_drawRectangle(&g_sContext, &box);
-
-    // We are now done writing to the display.  However, if we stopped here, we would not
-    // see any changes on the actual LCD.  This is because we need to send our changes
-    // to the LCD, which then refreshes the display.
-    // Since this is a slow operation, it is best to refresh (or "flush") only after
-    // we are done drawing everything we need.
-    Graphics_flushBuffer(&g_sContext);
-
-    dispThree[0] = ' ';
-    dispThree[2] = ' ';
-
-    while (1)    // Forever loop
+#pragma vector=TIMER2_A0_VECTOR
+__interrupt void TimerA2_ISR(void) {
+    timercount++;
+    delay_cnt++;
+    if (timercount == 60000)
+        timercount = 0;
+    if (timercount % 100 == 0)
     {
-        // Check if any keys have been pressed on the 3x4 keypad
-        currKey = getKey();
-        if (currKey == '*')
-            BuzzerOn();
-        if (currKey == '#')
-            BuzzerOff();
-        if ((currKey >= '0') && (currKey <= '9'))
-            setLeds(currKey - 0x30);
-
-        if (currKey)
-        {
-            dispThree[1] = currKey;
-            // Draw the new character to the display
-            Graphics_drawStringCentered(&g_sContext, dispThree, dispSz, 48, 55, OPAQUE_TEXT);
-
-            // Refresh the display so it shows the new data
-            Graphics_flushBuffer(&g_sContext);
-
-            // wait awhile before clearing LEDs
-            swDelay(1);
-            setLeds(0);
-        }
-
-    }  // end while (1)
+        increment_tm(global_time, 1);
+        adc_convert();
+    }
 }
 
+#pragma vector=ADC12_VECTOR
+__interrupt void ADC12_ISR(void) {
+    in_temp = ADC12MEM0;
+    in_wheel = ADC12MEM1;
+    temp_changed = 1;
+}
+
+
+// Main
+void main(void) {
+    volatile float temperatureDegC;
+    volatile float temperatureDegF;
+    volatile float degC_per_bit;
+    volatile unsigned int bits30, bits85;
+    // Stop WDT
+    WDTCTL = WDTPW | WDTHOLD;       // Stop watchdog timer
+
+    REFCTL0 &= ~REFMSTR;    // Reset REFMSTR to hand over control of
+    // internal reference voltages to
+    // ADC12_A control registers
+
+    P8SEL &= ~BIT0;
+    P8DIR |= BIT0;
+    P8OUT |= BIT0;
+
+    ADC12CTL0 = ADC12SHT0_9 | ADC12REFON | ADC12ON | ADC12MSC;     // Internal ref = 1.5V
+    ADC12CTL1 = ADC12SHP | ADC12CONSEQ_1;                     // Enable sample timer
+    // Using ADC12MEM0 to store reading
+    ADC12MCTL0 = ADC12SREF_1 + ADC12INCH_10;  // ADC i/p ch A10 = temp sense
+    ADC12MCTL1 = ADC12SREF_0 + ADC12INCH_5 + ADC12EOS;
+
+    __delay_cycles(100);                    // delay to allow Ref to settle
+    ADC12CTL0 |= ADC12ENC;                // Enable conversion
+    // Use calibration data stored in info memory
+    bits30 = CALADC12_15V_30C;
+    bits85 = CALADC12_15V_85C;
+    degC_per_bit = ((float)(85.0 - 30.0)) / ((float)(bits85 - bits30));
+
+    ADC12IE = BIT1;
+    _BIS_SR(GIE);
+
+    global_time = (struct tm*)malloc(sizeof(struct tm));
+    last_global_time = (struct tm*)malloc(sizeof(struct tm));
+    initialize_tm(global_time);
+    initialize_tm(last_global_time);
+    configDisplay();
+    configKeypad();
+    runtimerA2();
+
+    Graphics_clearDisplay(&g_sContext); // Clear the display
+
+    state_t state = S_RUN;
+    editstate_t editstate = E_MON;
+
+    time_str = (char*)malloc(sizeof(char) * 9);
+    last_time_str = (char*)malloc(sizeof(char) * 9);
+    date_str = (char*)malloc(sizeof(char) * 7);
+    temp_c_str = (char*)malloc(sizeof(char) * 8);
+    temp_f_str = (char*)malloc(sizeof(char) * 8);
+    convert_time(time_str, global_time);
+    convert_time(last_time_str, global_time);
+    convert_date(date_str, global_time);
+
+    volatile int btn;
+    const int mon_days[] =
+          {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    while(1) {
+        btn = getKey();
+
+        switch (state)
+                {
+                case S_RUN:
+                    if (btn == '1') {
+                        stoptimerA2();
+                        state = S_EDIT;
+                    }
+                    break;
+                case S_EDIT:
+                    if (btn == '2') {
+                        runtimerA2();
+                        state = S_RUN;
+                        editstate = E_MON;
+                        mktime(global_time);
+                        break;
+                    }
+                    adc_convert();
+                    switch (editstate) {
+                    case E_MON:
+                        if (btn == '1')
+                            editstate = E_DAY;
+                        global_time->tm_mon = map(in_wheel, 0, 4085, 0, 11);
+                        break;
+                    case E_DAY:
+                        if (btn == '1')
+                            editstate = E_HR;
+                        global_time->tm_mday = map(in_wheel, 0, 4085, 1, mon_days[global_time->tm_mon]);
+                        break;
+                    case E_HR:
+                        if (btn == '1')
+                            editstate = E_MIN;
+                        global_time->tm_hour = map(in_wheel, 0, 4085, 0, 23);
+                        break;
+                    case E_MIN:
+                        if (btn == '1')
+                            editstate = E_SEC;
+                        global_time->tm_min = map(in_wheel, 0, 4085, 0, 59);
+                        break;
+                    case E_SEC:
+                        if (btn == '1')
+                            editstate = E_MON;
+                        global_time->tm_sec = map(in_wheel, 0, 4085, 0, 59);
+                        break;
+                    }
+                    break;
+                }
+
+        if (temp_changed) {
+            temperatureDegC = (float) ((long) in_temp - CALADC12_15V_30C )
+                    * degC_per_bit + 30.0;
+            temperatureDegF = temperatureDegC * 9.0 / 5.0 + 32.0;
+            temp_changed = 0;
+        }
+
+        convert_temp(temp_c_str, temperatureDegC, 0);
+        convert_temp(temp_f_str, temperatureDegF, 1);
+        convert_time(time_str, global_time);
+        convert_date(date_str, global_time);
+        utc = mktime(global_time);
+
+        times[utc % 60] = utc;
+        tempC[utc % 60] = temperatureDegC;
+
+//        if (global_time->tm_sec != last_global_time->tm_sec) {
+            Graphics_clearDisplay(&g_sContext);
+            Graphics_drawStringCentered(&g_sContext, time_str, AUTO_STRING_LENGTH, 51, 10, TRANSPARENT_TEXT);
+            Graphics_drawStringCentered(&g_sContext, date_str, AUTO_STRING_LENGTH, 51, 20, TRANSPARENT_TEXT);
+            Graphics_drawStringCentered(&g_sContext, temp_c_str, AUTO_STRING_LENGTH, 51, 30, TRANSPARENT_TEXT);
+            Graphics_drawStringCentered(&g_sContext, temp_f_str, AUTO_STRING_LENGTH, 51, 40, TRANSPARENT_TEXT);
+            Graphics_flushBuffer(&g_sContext); // update display
+//            last_global_time->tm_sec = global_time->tm_sec;
+//        }
+    }
+}
+
+//void displayStuff(int count) {
+//    int monthDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+//    char month[12][4] = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
+//    int days = count/86400;
+//    int hours = (count%86400)/3600;
+//    int minutes = ((count%86400)%3600)/60;
+//    int seconds = (((count%86400)%3600)%60);
+//
+//    int months = 0;
+//    int totalDayMonths = 0;
+//    int tempTotalDayMonths = 31;
+//    int i = 0;
+//    while(tempTotalDayMonths < days) {
+//        totalDayMonths += monthDays[i];
+//        tempTotalDayMonths += monthDays[i+1];
+//        months++;
+//        i++;
+//    }
+//
+//    char *sDays[4];
+//    char *sHours[4];
+//    char *sMin[4];
+//    char *sSec[4];
+//
+//    toArray(days, sDays);
+//    toArray(hours, sHours);
+//    toArray(minutes, sMin);
+//    toArray(seconds, sSec);
+//
+//    days -= totalDayMonths;
+//
+//    char disp[9][5];
+//    disp[0] = month[months];
+//    disp[1] = ':';
+//    disp[2] = sDays;
+//    disp[3] = ':';
+//    disp[4] = sHours;
+//    disp[5] = ':';
+//    disp[6] = sMin;
+//    disp[7] = ':';
+//    disp[8] = sSec;
+//
+//    Graphics_clearDisplay(&g_sContext);
+//    Graphics_drawStringCentered(&g_sContext, disp, AUTO_STRING_LENGTH, 48, 15, TRANSPARENT_TEXT);
+//    Graphics_flushBuffer(&g_sContext); // update display
+//}
+
+void toArray(int i,char *s) { // Convert Integer to String
+    char *p;
+    p = s;
+    p[2]=i%10;
+    i-=p[2];
+    i/=10;
+    p[1]=i%10;
+    i-=p[1];
+    i/=10;
+    p[0]=i%10;
+    i-=p[0];
+    p[3] = 0; // mark end of string
+    p[2]+=0x30;
+    p[1]+=0x30;
+    p[0]+=0x30;
+}
+
+
+void adc_convert() {
+    ADC12CTL0 &= ~ADC12SC;  // clear the start bit
+    ADC12CTL0 |= ADC12SC;       // Sampling and conversion start
+}
+
+void runtimerA2(void) {
+    TA2CTL = TASSEL_1 + MC_1 + ID_0;
+    TA2CCR0 = 327;
+    TA2CCTL0 = CCIE;
+}
+
+void stoptimerA2(void) {
+    TA2CTL = MC_0;
+    TA2CCTL0 &= ~CCIE;
+}
+
+void a2delay(float numLoops) {
+    delay_cnt = 0; // our timer_cnt increments ~100 times per second
+    while(delay_cnt < numLoops){
+        //weee! we're waiting around and having fun :)
+    }
+}
 
 void swDelay(char numLoops)
 {
---
+++ stringstuff.h
@@ -0,0 +1,205 @@
+struct tm
+{
+    int tm_sec;      /* seconds after the minute   - [0,59]  */
+    int tm_min;      /* minutes after the hour     - [0,59]  */
+    int tm_hour;     /* hours after the midnight   - [0,23]  */
+    int tm_mday;     /* day of the month           - [1,31]  */
+    int tm_mon;      /* months since January       - [0,11]  */
+    int tm_year;     /* years since 1900                     */
+    int tm_wday;     /* days since Sunday          - [0,6]   */
+    int tm_yday;     /* days since Jan 1st         - [0,365] */
+    int tm_isdst;    /* Daylight Savings Time flag           */
+
+};
+
+inline unsigned long mktime(struct tm * time_ptr)
+{
+    const int mon_days[] =
+          {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if (time_ptr->tm_sec > 59)
+    {
+        time_ptr->tm_min += 1;
+        time_ptr->tm_sec = 0;
+    }
+    if (time_ptr->tm_min > 59)
+    {
+        time_ptr->tm_hour += 1;
+        time_ptr->tm_min = 0;
+    }
+    if (time_ptr->tm_hour > 23)
+    {
+        time_ptr->tm_yday += 1;
+        time_ptr->tm_mday += 1;
+        time_ptr->tm_hour = 0;
+        if (time_ptr->tm_mday > mon_days[time_ptr->tm_mon])
+        {
+            time_ptr->tm_mon += 1;
+            time_ptr->tm_mday = 1;
+        }
+    }
+    if (time_ptr->tm_mon > 11 || time_ptr->tm_yday > 365)
+    {
+        time_ptr->tm_mon = 0;
+        time_ptr->tm_yday = 0;
+        time_ptr->tm_mday = 1;
+    }
+    return time_ptr->tm_yday * 86400L + time_ptr->tm_hour * 3600L
+            + time_ptr->tm_min * 60L + time_ptr->tm_sec;
+};
+
+inline int convert_temp(char* dest, float temp, int units)
+{
+    int temp_i = (int)temp;
+    dest[0] = '0' + temp_i / 100;
+    dest[1] = '0' + temp_i / 10;
+    dest[2] = '0' + temp_i % 10;
+    dest[3] = '.';
+    temp_i = (int)(temp * 10);
+    dest[4] = '0' + temp_i % 10;
+    dest[5] = ' ';
+    if (units == 1)
+        dest[6] = 'F';
+    else
+        dest[6] = 'C';
+    dest[7] = '\0';
+    return 0;
+};
+
+inline int convert_time(char* dest, struct tm * time_ptr)
+{
+    dest[0] = '0' + time_ptr->tm_hour / 10;
+    dest[1] = '0' + time_ptr->tm_hour % 10;
+    dest[2] = ':';
+    dest[3] = '0' + time_ptr->tm_min / 10;
+    dest[4] = '0' + time_ptr->tm_min % 10;
+    dest[5] = ':';
+    dest[6] = '0' + time_ptr->tm_sec / 10;
+    dest[7] = '0' + time_ptr->tm_sec % 10;
+    dest[8] = '\0';
+    return 0;
+};
+
+/**
+ * @brief Creates the formatted date.
+ *
+ * @param dest The destination date. Must be already allocated.
+ * @param time_ptr The time count.
+ *
+ * @return 0 if success.
+ */
+inline int convert_date(char* dest, struct tm * time_ptr)
+{
+    switch (time_ptr->tm_mon)
+    {
+    case 0:
+        dest[0] = 'J';
+        dest[1] = 'a';
+        dest[2] = 'n';
+        break;
+    case 1:
+        dest[0] = 'F';
+        dest[1] = 'e';
+        dest[2] = 'b';
+        break;
+    case 2:
+        dest[0] = 'M';
+        dest[1] = 'a';
+        dest[2] = 'r';
+        break;
+    case 3:
+        dest[0] = 'A';
+        dest[1] = 'p';
+        dest[2] = 'r';
+        break;
+    case 4:
+        dest[0] = 'M';
+        dest[1] = 'a';
+        dest[2] = 'y';
+        break;
+    case 5:
+        dest[0] = 'J';
+        dest[1] = 'u';
+        dest[2] = 'n';
+        break;
+    case 6:
+        dest[0] = 'J';
+        dest[1] = 'u';
+        dest[2] = 'l';
+        break;
+    case 7:
+        dest[0] = 'A';
+        dest[1] = 'u';
+        dest[2] = 'g';
+        break;
+    case 8:
+        dest[0] = 'S';
+        dest[1] = 'e';
+        dest[2] = 'p';
+        break;
+    case 9:
+        dest[0] = 'O';
+        dest[1] = 'c';
+        dest[2] = 't';
+        break;
+    case 10:
+        dest[0] = 'N';
+        dest[1] = 'o';
+        dest[2] = 'v';
+        break;
+    case 11:
+        dest[0] = 'D';
+        dest[1] = 'e';
+        dest[2] = 'c';
+        break;
+    }
+    dest[3] = ' ';
+    dest[4] = '0' + time_ptr->tm_mday / 10;
+    dest[5] = '0' + time_ptr->tm_mday % 10;
+    dest[6] = '\0';
+    return 0;
+};
+
+inline void increment_tm(struct tm * time_ptr, int seconds)
+{
+    time_ptr->tm_sec += seconds;
+    mktime(time_ptr);
+};
+
+inline void initialize_tm(struct tm * time_ptr)
+{
+    const int mon_days[] =
+          {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    unsigned long rawtime = 8569460;
+    time_ptr->tm_yday = rawtime / 86400;
+    rawtime -= time_ptr->tm_yday * 86400;
+    time_ptr->tm_hour = rawtime / 3600;
+    rawtime -= time_ptr->tm_hour * 3600;
+    time_ptr->tm_min = rawtime / 60;
+    rawtime -= time_ptr->tm_min * 60;
+    time_ptr->tm_sec = rawtime;
+    time_ptr->tm_year = 0;
+    int yday = time_ptr->tm_yday;
+    if (yday <= mon_days[0])
+        time_ptr->tm_mon = 0;
+    else
+    {
+        int i;
+        for (i = 1; i <= 11; i++)
+        {
+            yday -= mon_days[i-1];
+            if (yday <= mon_days[i])
+            {
+                time_ptr->tm_mon = i;
+                break;
+            }
+        }
+    }
+    time_ptr->tm_mday = yday;
+    mktime(time_ptr);
+};
+
+inline long map(long x, long in_min, long in_max, long out_min, long out_max)
+{
+    (x > in_max) ? x = in_max : (x < in_min) ? x = in_min : 1;
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+};
