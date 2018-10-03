e8e37c2fe429ca8bbe91566832cd90d40785a89b
Nathan Rosenberg <robotincad@gmail.com>
2018-10-03 13:21:00 -0400

    Updated

--- main.c
+++ main.c
@@ -8,20 +8,16 @@ typedef enum {B_START, B_CHANGE} state_b;
 
 // Function Prototypes
 void swDelay(char numLoops);
-void adc_convert(void);
-void runtimerA2(void);
-void stoptimerA2(void);
-void a2delay(float numLoops);
-void toArray(int i, char *s);
-int read_push_button(void);
+void adc_con(void);
+void starttimerA2(void);
 
 // Declare globals here
 
 #define CALADC12_15V_30C  *((unsigned int *)0x1A1A)
 #define CALADC12_15V_85C  *((unsigned int *)0x1A1C)
 
-struct tm * global_time; //Global time counter.
-struct tm * last_global_time; //Global time counter.
+struct tm * global_time;
+struct tm * last_global_time;
 volatile unsigned int in_temp;
 volatile unsigned int in_wheel;
 volatile char temp_changed = 0;
@@ -68,26 +64,21 @@ void main(void) {
     volatile float temperatureDegF;
     volatile float degC_per_bit;
     volatile unsigned int bits30, bits85;
-    // Stop WDT
-    WDTCTL = WDTPW | WDTHOLD;       // Stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;
 
-    REFCTL0 &= ~REFMSTR;    // Reset REFMSTR to hand over control of
-    // internal reference voltages to
-    // ADC12_A control registers
+    REFCTL0 &= ~REFMSTR;
 
     P8SEL &= ~BIT0;
     P8DIR |= BIT0;
     P8OUT |= BIT0;
 
-    ADC12CTL0 = ADC12SHT0_9 | ADC12REFON | ADC12ON | ADC12MSC;     // Internal ref = 1.5V
-    ADC12CTL1 = ADC12SHP | ADC12CONSEQ_1;                     // Enable sample timer
-    // Using ADC12MEM0 to store reading
-    ADC12MCTL0 = ADC12SREF_1 + ADC12INCH_10;  // ADC i/p ch A10 = temp sense
+    ADC12CTL0 = ADC12SHT0_9 | ADC12REFON | ADC12ON | ADC12MSC;
+    ADC12CTL1 = ADC12SHP | ADC12CONSEQ_1;
+    ADC12MCTL0 = ADC12SREF_1 + ADC12INCH_10;
     ADC12MCTL1 = ADC12SREF_0 + ADC12INCH_0 + ADC12EOS;
 
-    __delay_cycles(100);                    // delay to allow Ref to settle
-    ADC12CTL0 |= ADC12ENC;                // Enable conversion
-    // Use calibration data stored in info memory
+    __delay_cycles(100);
+    ADC12CTL0 |= ADC12ENC;
     bits30 = CALADC12_15V_30C;
     bits85 = CALADC12_15V_85C;
     degC_per_bit = ((float)(85.0 - 30.0)) / ((float)(bits85 - bits30));
@@ -101,9 +92,9 @@ void main(void) {
     initialize_tm(last_global_time);
     configDisplay();
     configKeypad();
-    runtimerA2();
+    starttimerA2();
 
-    Graphics_clearDisplay(&g_sContext); // Clear the display
+    Graphics_clearDisplay(&g_sContext);
 
     state_b state = B_START;
 
@@ -134,10 +125,9 @@ void main(void) {
                     state = B_START;
                     break;
                 }
-                adc_convert();
+                adc_con();
                 long mapped = map(in_wheel, 0, 4095, 0, 256);
                 BuzzerSetPwm(mapped);
-//                BuzzerSetPwm(200);
                 break;
             }
 
@@ -147,7 +137,7 @@ void main(void) {
             for (i=0; i<4; i++) {
               temp_avg += temp_hist[i];
             }
-            temp_avg /= 4 ;  // divide by 4 by right shifting 2
+            temp_avg /= 4 ;
 
             temperatureDegC = (float) ((long) temp_avg - CALADC12_15V_30C )
                     * degC_per_bit + 30.0;
@@ -170,52 +160,27 @@ void main(void) {
         times[utc % 256] = utc;
         tempC[utc % 256] = temperatureDegC;
 
-//        if (global_time->tm_sec != last_global_time->tm_sec) {
-//            Graphics_clearDisplay(&g_sContext);
-            Graphics_drawStringCentered(&g_sContext, time_str, AUTO_STRING_LENGTH, 51, 10, OPAQUE_TEXT);
-            Graphics_drawStringCentered(&g_sContext, date_str, AUTO_STRING_LENGTH, 51, 20, OPAQUE_TEXT);
-            Graphics_drawStringCentered(&g_sContext, temp_c_str, AUTO_STRING_LENGTH, 51, 30, OPAQUE_TEXT);
-            Graphics_drawStringCentered(&g_sContext, temp_f_str, AUTO_STRING_LENGTH, 51, 40, OPAQUE_TEXT);
-            Graphics_flushBuffer(&g_sContext); // update display
-//            last_global_time->tm_sec = global_time->tm_sec;
-//        }
+        Graphics_drawStringCentered(&g_sContext, time_str, AUTO_STRING_LENGTH, 51, 10, OPAQUE_TEXT);
+        Graphics_drawStringCentered(&g_sContext, date_str, AUTO_STRING_LENGTH, 51, 20, OPAQUE_TEXT);
+        Graphics_drawStringCentered(&g_sContext, temp_c_str, AUTO_STRING_LENGTH, 51, 30, OPAQUE_TEXT);
+        Graphics_drawStringCentered(&g_sContext, temp_f_str, AUTO_STRING_LENGTH, 51, 40, OPAQUE_TEXT);
+        Graphics_flushBuffer(&g_sContext); // update display
     }
 }
 
 
-void adc_convert() {
-    ADC12CTL0 &= ~ADC12SC;  // clear the start bit
-    ADC12CTL0 |= ADC12SC;       // Sampling and conversion start
+void adc_con() {
+    ADC12CTL0 &= ~ADC12SC;
+    ADC12CTL0 |= ADC12SC;
 }
 
-void runtimerA2(void) {
+void starttimerA2(void) {
     TA2CTL = TASSEL_1 + MC_1 + ID_0;
     TA2CCR0 = 327;
     TA2CCTL0 = CCIE;
 }
 
-void stoptimerA2(void) {
-    TA2CTL = MC_0;
-    TA2CCTL0 &= ~CCIE;
-}
-
-void a2delay(float numLoops) {
-    delay_cnt = 0; // our timer_cnt increments ~100 times per second
-    while(delay_cnt < numLoops){
-        //weee! we're waiting around and having fun :)
-    }
-}
-
-void swDelay(char numLoops)
-{
-	// This function is a software delay. It performs
-	// useless loops to waste a bit of time
-	//
-	// Input: numLoops = number of delay loops to execute
-	// Output: none
-	//
-	// smj, ECE2049, 25 Aug 2013
-
+void swDelay(char numLoops) {
 	volatile unsigned int i,j;	// volatile to prevent removal in optimization
 			                    // by compiler. Functionally this is useless code
 
